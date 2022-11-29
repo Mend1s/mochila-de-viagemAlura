99f8e0154f5e35dadcb3dc6c53b198fc932ca671
Mend1s <andremendesskt99@gmail.com>
2022-11-29 11:38:44 -0300

    add

--- js/main.js
+++ js/main.js
@@ -15,14 +15,18 @@ form.addEventListener("submit", (evento) => {
 })
 
 function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li') // criando elemento do tipo 'li'
+    const novoItem = document.createElement("li") // criando elemento do tipo 'li'
     novoItem.classList.add("item") // adicionando css no elemento criado
 
-    const numeroItem = document.createElement('strong') // criando elemento do tipo 'strong'
+    const numeroItem = document.createElement("strong") // criando elemento do tipo 'strong'
     numeroItem.innerHTML = quantidade // inserindo quantidade na tag 'strong'
 
     novoItem.appendChild(numeroItem) // inserindo um elemento dentro de outro
     novoItem.innerHTML += nome //inserindo nome na tag 'li'
 
     lista.appendChild(novoItem)
+
+    console.log(novoItem)
+    // localStorage.setItem("nome", nome)
+    // localStorage.setItem("quantidade", quantidade)
 }

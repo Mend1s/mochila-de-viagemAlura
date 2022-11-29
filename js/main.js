9c3f716476e8d9327f004420b405a46eec7fdb3b
Mend1s <andremendesskt99@gmail.com>
2022-11-29 11:07:13 -0300

    add atualizações

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+      
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
+
+})
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li') // criando elemento do tipo 'li'
+    novoItem.classList.add("item") // adicionando css no elemento criado
+
+    const numeroItem = document.createElement('strong') // criando elemento do tipo 'strong'
+    numeroItem.innerHTML = quantidade // inserindo quantidade na tag 'strong'
+
+    novoItem.appendChild(numeroItem) // inserindo um elemento dentro de outro
+    novoItem.innerHTML += nome //inserindo nome na tag 'li'
+
+    lista.appendChild(novoItem)
+}

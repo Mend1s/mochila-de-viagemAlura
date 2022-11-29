08d61ed1daaba9de2231c21ee4b88d480b611973
Mend1s <andremendesskt99@gmail.com>
2022-11-29 12:05:19 -0300

    ajustando localstorage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-      
+
     criaElemento(nome.value, quantidade.value)
 
     nome.value = ""
@@ -24,9 +25,14 @@ function criaElemento(nome, quantidade) {
     novoItem.appendChild(numeroItem) // inserindo um elemento dentro de outro
     novoItem.innerHTML += nome //inserindo nome na tag 'li'
 
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
     lista.appendChild(novoItem)
 
-    console.log(novoItem)
-    // localStorage.setItem("nome", nome)
-    // localStorage.setItem("quantidade", quantidade)
+    localStorage.setItem("item", JSON.stringify(itens))
 }
